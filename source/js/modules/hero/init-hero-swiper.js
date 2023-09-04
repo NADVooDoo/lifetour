d1d4481f664284d6bcd8077c723b8cb217af6fe5
Nikolai Didur <nadvoodoo@gmail.com>
2023-09-04 16:44:22 +0300

    Исправляет зацикленный слайдер hero

--- source/js/modules/hero/init-hero-swiper.js
+++ source/js/modules/hero/init-hero-swiper.js
@@ -3,44 +3,27 @@ import {isMobileDevice} from '../../utils/is-mobile-device';
 
 const getHeroSlider = () => {
   const sliderElement = document.querySelector('[data-hero-slider]');
-
-  const createSlider = () => {
-    return new Swiper(sliderElement, {
-      allowTouchMove: isMobileDevice(),
-      direction: 'horizontal',
-      speed: 300,
-      pagination: {
-        el: '[data-hero-pagination]',
-        clickable: true,
-        renderBullet: (index, className) => {
-          return `<button class="hero__pagination-button ${className}" type="button" aria-label="Перейти к ${index + 1} слайду"></button>`;
-        },
-      },
-      on: {
-        init() {
-          pauseMediaOnSlide(this);
-        },
-        slideChange() {
-          pauseMediaOnSlide(this);
-        },
-      },
-    });
-  };
-
-  const pauseMediaOnSlide = (swiper) => {
-    const activeSlide = swiper.slides[swiper.activeIndex];
-    const mediaElements = activeSlide.querySelectorAll('video, audio');
-
-    mediaElements.forEach((media) => {
-      media.play();
-    });
-  };
-
-  if (sliderElement) {
-    return createSlider();
+  if (!sliderElement) {
+    return null;
   }
 
-  return null;
+  return new Swiper(sliderElement, {
+    loop: true,
+    breakpoints: {
+      768: {
+        loop: false,
+      },
+    },
+    allowTouchMove: isMobileDevice(),
+    speed: 300,
+    pagination: {
+      el: '[data-hero-pagination]',
+      clickable: true,
+      renderBullet: (index, className) => {
+        return `<button class="hero__pagination-button ${className}" type="button" aria-label="Перейти к ${index + 1} слайду"></button>`;
+      },
+    },
+  });
 };
 
 export {getHeroSlider};
