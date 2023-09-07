f9641c7f9290aeb82c01e3ef88d551c4366630f2
Nikolai Didur <nadvoodoo@gmail.com>
2023-09-07 13:06:32 +0300

    Исправляет часть багов после тестирования

--- source/js/modules/hero/init-hero-swiper.js
+++ source/js/modules/hero/init-hero-swiper.js
@@ -3,27 +3,22 @@ import {isMobileDevice} from '../../utils/is-mobile-device';
 
 const getHeroSlider = () => {
   const sliderElement = document.querySelector('[data-hero-slider]');
-  if (!sliderElement) {
-    return null;
+  if (sliderElement) {
+    return new Swiper(sliderElement, {
+      loop: true,
+      allowTouchMove: isMobileDevice(),
+      direction: 'horizontal',
+      speed: 300,
+      pagination: {
+        el: '[data-hero-pagination]',
+        clickable: true,
+        renderBullet: (index, className) => {
+          return `<button class="hero__pagination-button ${className}" type="button" aria-label="Перейти к ${index + 1} слайду"></button>`;
+        },
+      },
+    });
   }
-
-  return new Swiper(sliderElement, {
-    loop: true,
-    breakpoints: {
-      768: {
-        loop: false,
-      },
-    },
-    allowTouchMove: isMobileDevice(),
-    speed: 300,
-    pagination: {
-      el: '[data-hero-pagination]',
-      clickable: true,
-      renderBullet: (index, className) => {
-        return `<button class="hero__pagination-button ${className}" type="button" aria-label="Перейти к ${index + 1} слайду"></button>`;
-      },
-    },
-  });
+  return null;
 };
 
 export {getHeroSlider};
