f19e8e5ed9c34e54f02c7e81c331eea16ab39f57
Nikolai Didur <nadvoodoo@gmail.com>
2023-09-07 14:14:30 +0300

    Исправляет часть багов после тестирования

--- source/js/main.js
+++ source/js/main.js
@@ -29,7 +29,6 @@ window.addEventListener('DOMContentLoaded', () => {
   getHeight();
   const burger = new Burger();
   burger.init();
-  getHeroSlider();
   getReviewsSlider();
   getAdvantagesSlider();
   getGallerySlider();
@@ -38,6 +37,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    getHeroSlider();
     initVideo();
     initAudio();
     getToursSlider();
