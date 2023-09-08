c8b2fcf8036db53700cd6e6a197bd81832a27849
Nikolai Didur <nadvoodoo@gmail.com>
2023-09-08 13:37:05 +0300

    Исправляет все баги после тестирования

--- source/js/modules/hero/init-hero-swiper.js
+++ source/js/modules/hero/init-hero-swiper.js
@@ -7,8 +7,6 @@ const getHeroSlider = () => {
     return null;
   }
 
-  const listElement = sliderElement.querySelector('[data-hero-list]');
-
   const slider = new Swiper(sliderElement, {
     loop: true,
     breakpoints: {
@@ -25,27 +23,7 @@ const getHeroSlider = () => {
         return `<button class="hero__pagination-button ${className}" type="button" aria-label="Перейти к ${index + 1} слайду"></button>`;
       },
     },
-  });
-
-  document.addEventListener('keydown', (event) => {
-    if (event.key !== 'Tab' || event.ctrlKey || event.altKey) {
-      return;
-    }
-    setTimeout(() => {
-      const slideElement = document.activeElement.closest('[data-hero-slide]');
-      if (!slideElement) {
-        return;
-      }
-
-      const {left} = slideElement.getBoundingClientRect();
-      if (left) {
-        const index = parseInt(slideElement.dataset.heroSlide, 10);
-        slider.slideTo(index);
-
-        const [, currentTransform] = getComputedStyle(listElement).transform.match(/([-\d]+?),\s0\)$/);
-        listElement.style.transform = `translate3d(${parseInt(currentTransform, 10) - left}px, 0, 0)`;
-      }
-    }, 33);
+    watchSlidesProgress: true,
   });
 
   return slider;
